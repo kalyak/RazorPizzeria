2daeebe8b6fac0f439269733401fce11b6f51086
kalyakusumadi <kalyakusumadi@gmail.com>
2022-10-14 16:13:50 +0800

    Added pages, migrations. TODO: fix connection string to postgres

---
+++ RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Checkout
+{
+    [BindProperties(SupportsGet = true)]
+    public class CheckoutModel : PageModel
+    {
+        public string PizzaName { get; set; }
+        public float PizzaPrice {get;set;}
+        public string ImageTitle { get; set; }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrEmpty(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrEmpty(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+        }
+    }
+}
